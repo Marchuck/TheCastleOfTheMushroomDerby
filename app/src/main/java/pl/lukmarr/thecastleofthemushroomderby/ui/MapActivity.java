dad3a87209d7529d4a787928378d333d38281182
VaccumMan <lukmar93@o2.pl>
2015-12-20 17:59:41 +0100

    rx.subject refactored

--- app/src/main/java/pl/lukmarr/thecastleofthemushroomderby/ui/MapActivity.java
+++ app/src/main/java/pl/lukmarr/thecastleofthemushroomderby/ui/MapActivity.java
@@ -36,9 +36,21 @@ import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.functions.Func2;
 import rx.schedulers.Schedulers;
+import rx.subjects.PublishSubject;
+import rx.subjects.Subject;
 
 public class MapActivity extends AppCompatActivity {
     public static final String TAG = MapActivity.class.getSimpleName();
+    long time;
+
+    public MapActivity() {
+        super();
+        time = System.currentTimeMillis();
+        setupAdapters();
+        subjectRequest(++j);
+    }
+
+    private Subject<Figure, Figure> mSubject = PublishSubject.create();
 
     @Bind(R.id.tiles_frame_layout)
     TilesFrameLayout frameLayout;
@@ -54,7 +66,29 @@ public class MapActivity extends AppCompatActivity {
 
     @OnClick(R.id.character_image)
     public void oncc() {
+        time = System.currentTimeMillis();
         newRequest(++j);
+    }
+
+    private void subjectRequest(int j) {
+        client.getCharacter(j)
+                .subscribeOn(Schedulers.trampoline()).observeOn(AndroidSchedulers.mainThread())
+                .flatMap(new Func1<Hero, Observable<Response>>() {
+                    @Override
+                    public Observable<Response> call(final Hero hero) {
+                        String name = hero.getName().replaceAll(" ", "_");
+                        return wikiClient.getCharacterImage(name);
+                    }
+                }, new Func2<Hero, Response, Figure>() {
+                    @Override
+                    public Figure call(Hero hero, Response response) {
+                        Log.d(TAG, "hero : " + hero);
+                        final Figure figure = new Figure();
+                        figure.name = hero.getName();
+                        figure.image = getImage(response);
+                        return figure;
+                    }
+                }).subscribe(mSubject);
     }
 
     private void newRequest(int j) {
@@ -117,6 +151,57 @@ public class MapActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+                        textView.setText(figure.name);
+                        Log.d(TAG, "time elapsed: " + (System.currentTimeMillis() - time));
+                        Picasso.with(MapActivity.this).load(figure.image).fit().into(imaegView);
+                    }
+                });
+            }
+        }, new Action1<Throwable>() {
+            @Override
+            public void call(final Throwable throwable) {
+                throwable.printStackTrace();
+                Log.e(TAG, "time elapsed: " + (System.currentTimeMillis() - time));
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+//                        textView.setText(throwable.getMessage());
+                        Picasso.with(MapActivity.this)
+                                .load(Color.BLACK)
+                                .fit()
+                                .into(imaegView);
+                    }
+                });
+            }
+        });
+    }
+
+    int j = 0;
+    RestAdapter wikiAdapter;
+    StarWarsClient wikiClient;
+    StarWarsClient client;
+
+    void setupAdapters() {
+        wikiAdapter = new RestAdapter.Builder().setEndpoint(StarWarsClient.WIKI_ENDPOINT).build();
+        wikiClient = wikiAdapter.create(StarWarsClient.class);
+        GenericAdapter<Hero> heroGenericAdapter = new GenericAdapter<>(StarWarsClient.SWAPI_ENDPOINT, Hero.class);
+        client = heroGenericAdapter.getRestAdapter().create(StarWarsClient.class);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+        ButterKnife.bind(this);
+
+        mSubject.subscribe(new Action1<Figure>() {
+            @Override
+            public void call(final Figure figure) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(figure.name);
+                        Log.d(TAG, "time elapsed: " + (System.currentTimeMillis() - time));
                         Picasso.with(MapActivity.this).load(figure.image).fit().into(imaegView);
                     }
                 });
@@ -137,26 +222,6 @@ public class MapActivity extends AppCompatActivity {
                 });
             }
         });
-    }
-
-    int j = 0;
-    RestAdapter wikiAdapter;
-    StarWarsClient wikiClient;
-    StarWarsClient client;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-        ButterKnife.bind(this);
-
-        wikiAdapter = new RestAdapter.Builder().setEndpoint(StarWarsClient.WIKI_ENDPOINT).build();
-        wikiClient = wikiAdapter.create(StarWarsClient.class);
-
-        GenericAdapter<Hero> heroGenericAdapter = new GenericAdapter<>(StarWarsClient.SWAPI_ENDPOINT, Hero.class);
-        client = heroGenericAdapter.getRestAdapter().create(StarWarsClient.class);
-        newRequest(++j);
-
         frameLayout.setOnAnimationFinishedListener(new TilesFrameLayoutListener() {
             @Override
             public void onAnimationFinished() {
